7d76e073bf89636c5f4ab1af25381c8a5cba1e6d
root <admin@example.com>
2025-01-16 11:25:45 +0800

    修改

--- common/src/main/java/com/ronllan/common/utils/BaseUtils.java
+++ common/src/main/java/com/ronllan/common/utils/BaseUtils.java
@@ -1,11 +1,21 @@
 package com.ronllan.common.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.UUID;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.mock.web.MockMultipartFile;
 
 public class BaseUtils {
 	
 	public static String getUuid(){
 		return UUID.randomUUID().toString();
+	}
+	
+	public static MultipartFile byteToMultipartFile(byte[] bytes) throws IOException{
+		return new MockMultipartFile("multipartFile",IOUtils.toByteArray(new ByteArrayInputStream(bytes)));
 	}
 	
 }
