0e9ecb4311061146dc0705df176c010071a68439
root <admin@example.com>
2025-01-17 14:05:31 +0800

    修改

--- admin/src/main/java/com/ronllan/modules/sys/dao/SysNoticeDao.java
+++ admin/src/main/java/com/ronllan/modules/sys/dao/SysNoticeDao.java
@@ -5,9 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 import com.ronllan.common.dao.BaseDao;
 import com.ronllan.modules.sys.entity.SysNoticeEntity;
 
-import java.util.List;
-import java.util.Map;
-
 /**
 * 通知管理
 *
@@ -15,13 +12,5 @@ import java.util.Map;
 */
 @Mapper
 public interface SysNoticeDao extends BaseDao<SysNoticeEntity> {
-    /**
-     * 获取被通知的用户列表
-     */
-    List<SysNoticeEntity> getNoticeUserList(Map<String, Object> params);
-
-    /**
-     * 获取我的通知列表
-     */
-    List<SysNoticeEntity> getMyNoticeList(Map<String, Object> params);
+	
 }
